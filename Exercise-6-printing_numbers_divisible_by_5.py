8a560c7ac817b35949fb5172fc94a0c5876591c3
unknown <miggy.payumo26@gmail.com>
2024-01-26 11:15:45 +0800

    Code labeled and finalized

--- Exercise-6-printing_numbers_divisible_by_5.py
+++ Exercise-6-printing_numbers_divisible_by_5.py
@@ -3,10 +3,13 @@
 # 2. Evaluate the numbers in the sample set
 # 3. Print those divisible by 5
 
+# Sample number set.
 number_set = [12, 25, 15, 4, 27, 5]
 
+# For printing the sample set.
 print("Sample set is: ", number_set)
 
+# For printing those only divisible by 5.
 print("These are the only number/s divisible by 5: ")
 for num in number_set:
     if num % 5 == 0:
